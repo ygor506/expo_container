6a10361cd4e8163ee9c7bf9e11985822131f73fa
ygor <ygor.marquezini@escola.pr.gov.br>
2025-11-25 11:14:21 +0000

    44

--- componentes/CartaoUsuario.js
+++ componentes/CartaoUsuario.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image,} from "react-native";
+import { View, Text, Image,StyleSheet} from "react-native";
 import fotos from '../img/fotos';
 
 export default function CartaoUsuario({nome, descricao, img}){
