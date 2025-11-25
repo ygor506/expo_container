9244def765a48662e2e8693c8522f38c31b93a98
ygor <ygor.marquezini@escola.pr.gov.br>
2025-11-25 11:25:16 +0000

    33

--- App.js
+++ App.js
@@ -31,6 +31,13 @@ export default function App(){
       descricao='Programador mobile'
       img='perfil00'
       />
+
+
+            <CartaoUsuario 
+      nome='pedro silva'
+      descricao='Programador mobile'
+      img='perfil00'
+      />
   </ScrollView>
    
 
